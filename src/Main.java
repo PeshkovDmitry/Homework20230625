661441ca998b012e6aa2a52212283f9a84ad6322
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-06-26 20:20:44 +0300

    Решена задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println(Task01("{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}"));
+//        System.out.println(task01("{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}"));
+        task02(new int[] {5,8,1,2,7,10,8,1,22,5,12,3,1});
     }
 
     /*
@@ -13,7 +17,7 @@ public class Main {
     select * from students where 'name=Ivanov' and 'country=Russia' and...
      */
 
-    public static String Task01(String jsonStr) {
+    public static String task01(String jsonStr) {
         StringBuilder stringBuilder = new StringBuilder("select * from students where ");
         jsonStr = jsonStr.replaceAll("\\{|\\}|\"","");
         String[] pairs = jsonStr.split(",");
@@ -32,4 +36,47 @@ public class Main {
     результат после каждой итерации запишите в лог-файл.
      */
 
+    public static void task02(int[] arr) {
+        String logpath = "log.txt";
+        Logger logger = Logger.getAnonymousLogger();
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler(logpath);
+            logger.addHandler(fileHandler);
+            SimpleFormatter simpleFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(simpleFormatter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Sorting array: ").append(printArr(arr));
+        logger.info(stringBuilder.toString());
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    stringBuilder = new StringBuilder(String.format("Replacing %d and %d. Now: ", arr[j],arr[j + 1]));
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    stringBuilder.append(printArr(arr));
+                    logger.info(stringBuilder.toString());
+                }
+            }
+        }
+        stringBuilder = new StringBuilder("Result: ");
+        stringBuilder.append(printArr(arr));
+        logger.info(stringBuilder.toString());
+        fileHandler.close();
+        logger.getHandlers()[0].close();
+    }
+
+    public static String printArr(int[] arr) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i : arr) {
+            if (stringBuilder.length() > 0)
+                stringBuilder.append(", ");
+            stringBuilder.append(i);
+        }
+        return stringBuilder.toString();
+    }
 }
